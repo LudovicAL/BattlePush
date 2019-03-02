e1cb067887af29f5911a4b7e3ff200df25543df9
Jim <jimmyetlim@gmail.com>
2019-03-02 18:05:49 -0500

    Ajoute Menu et starting screen

---
+++ Assets/Scripts/Singletons/StartingManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartingManager : MonoBehaviour
+{
+    public Text countDown;
+
+    private IEnumerator startCountDown()
+    {
+        for (int i = 3; i >= 0; i--)
+        {
+            countDown.text = (i==0)?"Let's Go!":i.ToString();
+            yield return new WaitForSeconds(0.9f);
+        }
+        nextState();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        countDown.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameStatesManager.Instance.gameState.Equals(GameStatesManager.AvailableGameStates.Starting)) {
+            StartCoroutine(startCountDown());
+        }
+    }
+    private void nextState() {
+        GameStatesManager.Instance.ChangeGameStateTo(GameStatesManager.AvailableGameStates.Playing);
+    } 
+}
